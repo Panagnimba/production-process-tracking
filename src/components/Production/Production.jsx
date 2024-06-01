de1a84d9e4ef692e2f19cef38a736e5cd12c0b17
unknown <panagnimba@gmail.com>
2024-06-01 06:50:21 +0200

    [01-06-2024]Feat Production and graphics line, bar and pie charts

--- src/components/Production/Production.jsx
+++ src/components/Production/Production.jsx
@@ -1,31 +1,58 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Production.css"
 import NewProduction from "./NewProduction";
 import ProductionTable from "../Production/ProductionTable";
 import ProductionGraphs from "../Production/ProductionGraphs"
- 
+
 export default function Production() {
-    let [activeButtonIdx,setActiveButtonIdx] = useState(1)
-    function buttonClicked(e){
+    let [activeButtonIdx, setActiveButtonIdx] = useState(1)
+    let [filtreOption,setFiltreOption] = useState("hour")
+    let [typeOption,setTypeOption] = useState("line")
+
+    function buttonClicked(e) {
         setActiveButtonIdx(e.target.id)
     }
-    function renderContent(){
-        if(activeButtonIdx == 0 ) return <NewProduction></NewProduction>
-        if(activeButtonIdx == 1 ) return <ProductionTable></ProductionTable>
-        if(activeButtonIdx == 2 ) return <ProductionGraphs></ProductionGraphs>
+    function handleFiltreChange(e){
+        console.log(e.target.value)
+        setFiltreOption(e.target.value)
+    }
+    function handleTypeChange(e){
+        console.log(e.target.value)
+        setTypeOption(e.target.value)
+    }
+    function renderContent() {
+        if (activeButtonIdx == 0) return <NewProduction></NewProduction>
+        if (activeButtonIdx == 1) return <ProductionTable></ProductionTable>
+        if (activeButtonIdx == 2) return <ProductionGraphs filtreOption={filtreOption} typeOption={typeOption}></ProductionGraphs>
     }
     return (
         <div className="article-wrapper">
             <div className="article-top">
-                <button id="0" className={`button ${activeButtonIdx == 0 ? 'active':null}`} onClick={(e)=>buttonClicked(e)}>Lancer une opération de production</button>
-                <button id="1" className={`button ${activeButtonIdx == 1 ? 'active':null}`} onClick={(e)=>buttonClicked(e)}>Production en cours</button>
-                <button id="2" className={`button ${activeButtonIdx == 2 ? 'active':null}`} onClick={(e)=>buttonClicked(e)}>Consulter le graphique</button>
+                <button id="0" className={`button ${activeButtonIdx == 0 ? 'active' : null}`} onClick={(e) => buttonClicked(e)}>Lancer une opération de production</button>
+                <button id="1" className={`button ${activeButtonIdx == 1 ? 'active' : null}`} onClick={(e) => buttonClicked(e)}>Production en cours</button>
+                <button id="2" className={`button ${activeButtonIdx == 2 ? 'active' : null}`} onClick={(e) => buttonClicked(e)}>Consulter le graphique</button>
+                <div className="button">
+                    <label htmlFor="filtre">Type</label>
+                    <select name="filtre" id="" onChange={handleTypeChange} required className="form-control">
+                        <option value="line" >Line chart</option>
+                        <option value="bar" >Bar chart</option>
+                        <option value="pie" >Pie chart</option>
+                    </select>
+                </div>
+                <div className="button">
+                    <label htmlFor="filtre">Filtrer</label>
+                    <select name="filtre" id="" onChange={handleFiltreChange} required className="form-control">
+                        <option value="hour" >par heure</option>
+                        <option value="day" >par jour</option>
+                        <option value="month" >par mois</option>
+                    </select>
+                </div>
             </div>
             <div className="article-content">
                 {
                     renderContent()
                 }
-                
+
             </div>
         </div>
     )
--- src/components/Production/ProductionGraphs.jsx
+++ src/components/Production/ProductionGraphs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,8 +9,9 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import 'chart.js/auto';  // Automatically registers the necessary chart components
 
+import { Doughnut, Line, Bar, Pie } from 'react-chartjs-2';
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -20,41 +21,74 @@ ChartJS.register(
   Tooltip,
   Legend
 );
+import axiosInstance from '../../axios/axios';
 
-const ChartComponent = () => {
+const LineChartComponent = ({ filtreOption, typeOption }) => {
+  let [labels, setLabels] = useState([])
+  let [values, setValues] = useState([])
+  useEffect(() => {
+    async function fetchLineChart() {
+      let resp = await axiosInstance.get(`/production/line/${filtreOption}`)
+      if (resp.data.success) {
+        console.log(resp.data.result)
+        setLabels(resp.data.result.labels)
+        setValues(resp.data.result.values)
+
+      }
+    }
+    fetchLineChart()
+  }, [filtreOption])
   const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: labels,
     datasets: [
       {
-        label: 'My First dataset',
-        fill: false,
+        label: 'Production en cours',
         lineTension: 0.1,
         backgroundColor: 'rgba(75,192,192,0.4)',
         borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40]
+        // backgroundColor: [
+        //   'rgba(75,192,192,0.4)',
+        //   'rgba(254, 108, 0, 0.6)', // clr-pumpkin
+        //   'rgba(168, 165, 166, 0.6)', // clr-silver
+        //   'rgba(45, 160, 73, 0.6)', // clr-green
+        //   'rgba(254, 200, 10, 0.6)', // clr-yellow
+        //   'rgba(48, 41, 36, 0.6)', // clr-jet
+        //   'rgba(255, 195, 151, 0.6)' // clr-peach
+        // ],
+        // borderColor: [
+        //   'rgba(75,192,192,0.4)',
+        //   'rgba(254, 108, 0, 1)', // clr-pumpkin
+        //   'rgba(168, 165, 166, 1)', // clr-silver
+        //   'rgba(45, 160, 73, 1)', // clr-green
+        //   'rgba(254, 200, 10, 1)', // clr-yellow
+        //   'rgba(48, 41, 36, 1)', // clr-jet
+        //   'rgba(255, 195, 151, 1)' // clr-peach
+        // ],
+        data: values
       }
     ]
   };
-
   return (
-    <div className="chart-container" style={{width:"100%"}}>
-      <h2>Line Example</h2>
-      <Line data={data} />
+    <div className="chart-container" style={{ width: "100%", maxHeight: "500px" }}>
+      <h2>Production Horaire</h2>
+      <div style={{ display: "flex", justifyContent: "center", width: "100%", maxHeight: "500px" }}>
+        {
+          typeOption == "line" && <Line data={data} />
+        }
+        {
+          typeOption == "bar" && <Bar data={data} />
+        }
+        {
+          typeOption == "pie" && <Pie data={data} />
+        }
+
+      </div>
+
+
+
+
     </div>
   );
 };
 
-export default ChartComponent;
+export default LineChartComponent;
